9c64018e8b5b550e6c2935f304a1b3a5c8512fc2
levelp <onojasteven13@gmail.com>
2026-06-20 23:19:12 +0100

    change

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -108,8 +108,8 @@ resource "aws_security_group" "ecs_sg" {
   description = "Allow SSH and HTTPS"
   vpc_id      = aws_vpc.vpc.id
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 3000
+    to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
